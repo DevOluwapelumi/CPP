e72838d09ea5fe0fadf2fdee2854c5fa06e8a5a0
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-03-20 16:18:08 +0100

    initial commit

---
+++ index.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+    std::cout << "Welcome to C++ class. This class promised to be exciting."<<std::end1;
+    std::cout << "This is the first class. It started late due to some technical issues \n";
+    return 0;
+}
+
+
